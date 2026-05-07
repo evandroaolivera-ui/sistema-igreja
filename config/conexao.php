363325c090c18aede5ebb136abbc52fb29819227
evandroaolivera-ui <evandro.a.olivera@gmail.com>
2026-05-07 16:42:56 -0300

    Update conexao.php

--- config/conexao.php
+++ config/conexao.php
@@ -1,6 +1,6 @@
 <?php
 
-$host = "dpg-d7udiireo5us73cvg7k0-a";
+$host = "dpg-d7udiireo5us73cvg7k0-a.render.com";
 $dbname = "igreja_db_wams";
 $user = "igreja_db_wams_user";
 $password = "lC9jH9W3cJJ0QXrk1sJBUr3Hoy2fqfJ0";
