603c5f6a06def9b88074d52acd989b51bdc9848c
evandroaolivera-ui <evandro.a.olivera@gmail.com>
2026-05-07 16:50:18 -0300

    Update conexao.php

--- config/conexao.php
+++ config/conexao.php
@@ -1,6 +1,6 @@
 <?php
 
-$host = "postgresql://igreja_db_wams_user:lC9jH9W3cJJ0QXrk1sJBUr3Hoy2fqfJ0@dpg-d7udiireo5us73cvg7k0-a/igreja_db_wams";
+$host = "@dpg-d7udiireo5us73cvg7k0-a.oregon-postgres.render.com";
 $dbname = "igreja_db_wams";
 $user = "igreja_db_wams_user";
 $password = "lC9jH9W3cJJ0QXrk1sJBUr3Hoy2fqfJ0";
